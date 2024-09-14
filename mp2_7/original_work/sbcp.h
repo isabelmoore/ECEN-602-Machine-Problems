9af84940495b147b9901faed576c23cf70b4c3f8
Yiyang Yan <yyy2000@tamu.edu>
2024-09-14 17:56:46 -0400

    update

---
+++ mp2_7/original_work/sbcp.h
@@ -0,0 +1,53 @@
+#ifndef SBCP_H
+#define SBCP_H
+
+#include <stdint.h>
+
+/* SBCP Header Types */
+#define SBCP_HDR_JOIN   2
+#define SBCP_HDR_FWD    3
+#define SBCP_HDR_SEND   4
+
+/* SBCP Attribute Types */
+#define SBCP_ATTR_REASON        1
+#define SBCP_ATTR_USERNAME      2
+#define SBCP_ATTR_CLIENT_COUNT  3
+#define SBCP_ATTR_MESSAGE       4
+
+/* SBCP Header */
+struct sbcp_hdr {
+    uint16_t version_type;
+    uint16_t length;
+    uint8_t  *payload;
+};
+
+/* SBCP Attribute */
+struct sbcp_attr {
+    uint16_t type;
+    uint16_t length;
+    uint8_t  *payload;
+};
+
+/**
+ * @brief Returns SBCP header version.
+ * 
+ * @param hdr The pointer to the SBCP header
+ * 
+ * @return SBCP header version
+ */
+uint8_t sbcp_hdr_get_version(struct sbcp_hdr *hdr) {
+    return (hdr->version_type >> 7);
+}
+
+/**
+ * @brief Returns SBCP header type.
+ * 
+ * @param hdr The pointer to the SBCP header
+ * 
+ * @return SBCP header type
+ */
+uint8_t sbcp_hdr_get_type(struct sbcp_hdr *hdr) {
+    return (hdr->version_type &= 0x007F);
+}
+
+#endif /* SBCP_H */
